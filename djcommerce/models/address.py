85eec5d833b012a7384954c2fa1e4fe02139e682
Tanner <tannersiciliano@gmail.com>
2019-06-26 15:51:31 -0700

    Add address to docs

--- djcommerce/models/address.py
+++ djcommerce/models/address.py
@@ -4,8 +4,7 @@ from localflavor.us.models import USStateField, USZipCodeField
 
 class Address(models.Model):
     address_line_1 = models.CharField(max_length = 150)
-    address_line_2 = models.CharField(max_length = 150)
+    address_line_2 = models.CharField(max_length = 150, blank = True, null = True)
     city = models.CharField()
     state = USStateField()
     zip = USZipCodeField()
-    
