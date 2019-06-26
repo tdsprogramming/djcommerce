4c7fa04190f9813c5e9b69552eed51b0c60a8c19
Tanner <tannersiciliano@gmail.com>
2019-06-26 16:12:51 -0700

    Add configuration

---
+++ djcommerce/models/configuration.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class ConfigurationOption(models.Model):
+    description = models.CharField(max_length = 50)
+    
+class Configuration(models.Model):
+    name = models.CharField(max_length = 15)
+    options = models.ManyToManyField(ConfigurationOption, related_name = 'configurations')
