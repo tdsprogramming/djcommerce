ffecfceaa396503b2c6e3f387637b1f0b081dcdc
JWHProgramming <JWHProgramming@gmail.com>
2019-06-26 17:23:13 -0700

    First Views Creation Commit

---
+++ djcommerce/views/category.py
@@ -0,0 +1,23 @@
+from django.views.generic import CreateView,DetailView,DeleteView,UpdateView,ListView
+from models import Category
+
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = [
+        'name'
+    ]
+
+class CategoryDetailView(DetailView):
+    model = Category
+
+class CategoryListView(ListView):
+    model = Category
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = [
+        'name'
+    ]
+
+class CategoryDeleteView(DeleteView):
+    model = Category
