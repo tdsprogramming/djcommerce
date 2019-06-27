deb98035293b87e57bafa94db442aa85b641d8fd
Tanner <tannersiciliano@gmail.com>
2019-06-26 17:15:16 -0700

    Edit Profile with user

--- djcommerce/models/profile.py
+++ djcommerce/models/profile.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.conf import settings.AUTH_USER_MODEL
 
 from django_extensions.db.models import TimeStampedModel
 
+from .address import Address
+
 class Profile(TimeStampedModel):
-    pass
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE
+    )
+    addresses = ManyToManyField(Address)
+    
